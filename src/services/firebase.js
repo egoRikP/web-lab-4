016ba7074d1b9d934d1e942ff296b5e2da7efe54
egorik <flinksyt@gmail.com>
2026-04-19 01:54:11 +0300

    add firebase with app and auth config

---
+++ src/services/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_firebase_apiKey,
+  authDomain: process.env.REACT_APP_firebase_authDomain,
+  projectId: process.env.REACT_APP_firebase_projectId,
+  storageBucket: process.env.REACT_APP_firebase_storageBucket,
+  messagingSenderId: process.env.REACT_APP_firebase_messagingSenderId,
+  appId: process.env.REACT_APP_firebase_appId,
+  measurementId: process.env.REACT_APP_firebase_measurementId,
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
