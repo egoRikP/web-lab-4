f5d77a08c7b1e69da01be4d387b06dec3a03d812
egorik <flinksyt@gmail.com>
2026-04-20 12:23:17 +0300

    add hashtag router

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import "./App.css";
 
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import { DataProvider } from "./context/DataContext.js";
 
 import NavigationBar from "../src/components/NavigationBar.js";
